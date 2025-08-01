c20be85d61a08904e87b9cdd5b511e246efdb5c4
Jorge Jimenez <georgeguitarra07@gmail.com>
2025-08-01 03:01:44 +0200

    feat: Added favicon, logo and new font family

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,1 +1,2 @@
+export { UI } from "./ui.constants"
 export { PAGE_URLS } from "./page-urls.constants"
---
+++ src/lib/constants/ui.constants.ts
@@ -0,0 +1,7 @@
+export const UI = {
+  IMAGES: {
+    LOGO: "/img/logo.svg"
+  }
+}
+
+Object.freeze(UI)
